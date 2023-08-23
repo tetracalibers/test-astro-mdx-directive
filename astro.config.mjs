e0cde108e919a4568bbe1ecd8f314226ca955976
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-23 15:28:23 +0900

    integrationを登録

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+// @ts-check
+
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import AutoImport from "astro-auto-import"
+import { asideAutoImport, astroAsides } from "./integrations/mdx-directive"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
-});
+  integrations: [
+    AutoImport({
+      imports: [asideAutoImport]
+    }),
+    astroAsides(),
+    mdx()
+  ]
+})
